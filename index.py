fe1f46c33094ad027ae858974e58cc256f154f47
André <agmphandre@gmail.com>
2023-09-18 18:56:37 -0300

    Prjeto kivy alteraçao

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 # importar o App, builder (GUI)
-## criar o nosso aplicativo
-## criar a funçao build
+# criar o nosso aplicativo
+# criar a funçao build
 
 
 from kivy.app import App
@@ -14,18 +14,37 @@ class MeuAplicativo(App):
         return GUI
     
     def on_start(self):
-        self.pegar_cotacao("USD")
-        self.root.ids["moeda1"].text = f"Dólar R${self.pegar_cotacao('USD')}"
-        self.root.ids["moeda2"].text = f"Euro R${self.pegar_cotacao('EUR')}"
-        self.root.ids["moeda3"].text = f"Bitcoin R${self.pegar_cotacao(':BTC')}"
-        self.root.ids["moeda4"].text = f"Rúpia Indiana R${self.pegar_cotacao('INR')}"
+        self.root.ids.moeda1.text = f"Dólar: R${self.pegar_cotacao('USD')}"
+        self.root.ids.moeda2.text = f"Euro: R${self.pegar_cotacao('EUR')}"
+        self.root.ids.moeda3.text = f"Bitcoin: R${self.pegar_cotacao('BTC')}"
         
-    def pegar_cotacao(Self, moeda):
+    def pegar_cotacao(self, moeda):
         link = f"https://economia.awesomeapi.com.br/last/{moeda}-BRL"
         requisicao = requests.get(link)
-        print(requisicao.json())
-        cotacao = dic_requisicao[f"{moeda}BRL"]["bid"]
-        return cotacao
-MeuAplicativo().run()   
+        data = requisicao.json()
+        
+        if f"{moeda}-BRL" in data:
+            cotacao = data[f"{moeda}-BRL"]["bid"]
+            return cotacao
+        else:
+            return "N/A"
+        
+        
+    def atualizar_cotacoes(self):
+        moedas = {
+            "moeda1": {"nome": "Dólar", "codigo": "USD"},
+            "moeda2": {"nome": "Euro", "codigo": "EUR"},
+            "moeda3": {"nome": "Bitcoin", "codigo": "BTC"}
+        }
+        
+        for moeda_id, info in moedas.items():
+            cotacao = self.pegar_cotacao(info["codigo"])
+            self.root.ids[moeda_id].text = f"{info['nome']}: R${cotacao}" 
+        
+        print("Programa rodou com sucesso !")
+
+if __name__ == '__main__':
+    MeuAplicativo().run()
+   
     
    
